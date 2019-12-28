fc395ade6c13d34cab4269de2075017afc3b5b2f
ShotaroOkada <b1016117@gmail.com>
2019-12-28 14:54:52 +0900

    通信の並列処理完了の確認まで

--- src/drivers/firebase/operation.py
+++ src/drivers/firebase/operation.py
@@ -1,9 +1,12 @@
 from app import db
 import pprint
 import asyncio
+import time
 
 
-async def read_from_db(key):
+async def read_from_db(key, sec):
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, time.sleep, sec)
     try:
         data = db.child("evaluations").child(key).get()
     except Exception as error:
@@ -13,11 +16,24 @@ async def read_from_db(key):
         return data.val()
 
 
-data = read_from_db("appropriate")
-loop = asyncio.get_event_loop()
-result = loop.run_until_complete(data)
+async def main():
+    data1 = asyncio.create_task(
+        read_from_db("appropriate", 2)
+    )
+    data2 = asyncio.create_task(
+        read_from_db("stronger", 2)
+    )
+    print(f"started at {time.strftime('%X')}")
 
-pprint.pprint(result)
+    await data1
+    await data2
+
+    print(data1.result())
+    print(data2.result())
+
+    print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(main())
 
 # try:
 #     x = db.child("users").child("b1016118").get()
