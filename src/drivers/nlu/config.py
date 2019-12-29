e4295e11925e50775cb84295c190c0e1686fbbec
ShotaroOkada <b1016117@gmail.com>
2019-12-29 10:53:43 +0900

    gitignore設定

--- src/drivers/nlu/config.py
+++
@@ -1,2 +0,0 @@
-Natural_language_understanding_api_key = '0UD9fwWYsOmb3E3SWLhNJMGzo3TbwD85qi-pC-FWipxZ'
-Natural_language_understanding_url = 'https://gateway.watsonplatform.net/natural-language-understanding/api'
