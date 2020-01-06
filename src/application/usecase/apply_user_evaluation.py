ff53fabdb110f1d9679c96b4ad439217969b5c2a
ShotaroOkada <b1016117@gmail.com>
2020-01-06 14:29:31 +0900

    前回の評価がよくなかった文章に関しては、feedbackのアルゴリズムのみで返すよう修正

--- src/application/usecase/apply_user_evaluation.py
+++ src/application/usecase/apply_user_evaluation.py
@@ -10,15 +10,20 @@ import json
 
 
 def apply_user_evaluation(user_id, user_evaluations):
-    emotion_parameter = read_emotion_parameter(user_id)
-    feedback_emotion_parameter = read_feedback_emotion_parameter(user_id)
     entity_evaluations = read_evaluations()
     entity_emotions = read_emotions()
     feature_words = morphological_analysis(
         user_evaluations['nlu_algo']['text'])
-    new_emotion_parameter = update_emotion_parameter_to_user_evaluation(
-        emotion_parameter, user_evaluations['emotion_parameter_algo'], entity_evaluations, entity_emotions, feature_words)
+    feedback_emotion_parameter = read_feedback_emotion_parameter(user_id)
     new_feedback_emotion_parameter = update_emotion_parameter_to_user_evaluation(
         feedback_emotion_parameter, user_evaluations['feedback_algo'], entity_evaluations, entity_emotions, feature_words)
-    update_emotion_parameter(user_id, new_emotion_parameter)
     update_feedback_emotion_parameter(user_id, new_feedback_emotion_parameter)
+
+    if user_evaluations['emotion_parameter_algo'] == None:
+        return
+
+    emotion_parameter = read_emotion_parameter(user_id)
+    new_emotion_parameter = update_emotion_parameter_to_user_evaluation(
+        emotion_parameter, user_evaluations['emotion_parameter_algo'], entity_evaluations, entity_emotions, feature_words)
+
+    update_emotion_parameter(user_id, new_emotion_parameter)
--- src/application/usecase/estimate_emotion.py
+++ src/application/usecase/estimate_emotion.py
@@ -9,24 +9,38 @@ from src.adapters.gateway.feedback_emotion_parameter.read_feedback_emotion_param
 
 
 def estimate_emotion(user_id, text_info):
-    emotion_parameter = read_emotion_parameter(user_id)
     emotions = read_emotions()
     feedback_emotion_parameter = read_feedback_emotion_parameter(user_id)
     feature_words = morphological_analysis(text_info['text'])
     english_text = translate_english(text_info['text'])
     nlu_emotion_score = nlu_estimation_emotion(english_text)
-    emotion_score_applied_parameter = apply_emotion_parameter_to_nlu_score(
-        nlu_emotion_score, feature_words, emotion_parameter)
     emotion_score_applied_feedback_parameter = apply_emotion_parameter_to_nlu_score(
         nlu_emotion_score, feature_words, feedback_emotion_parameter
     )
+    feedback_parameter_estimated_emotion = convert_score_to_emotion_category_and_name(
+        emotion_score_applied_feedback_parameter, emotions
+    )
+
+    if text_info['previous_flag']:
+        response_object = {
+            "feedback_algo": {
+                "algorithm_id": "feedback_algo",
+                "text": text_info['text'],
+                "previous_flag": text_info['previous_flag'],
+                "emotion_category": feedback_parameter_estimated_emotion['category'],
+                "emotion_name": feedback_parameter_estimated_emotion['name']
+            }
+        }
+
+        return response_object
+
+    emotion_parameter = read_emotion_parameter(user_id)
+    emotion_score_applied_parameter = apply_emotion_parameter_to_nlu_score(
+        nlu_emotion_score, feature_words, emotion_parameter)
     nlu_estimated_emotion = convert_score_to_emotion_category_and_name(
         nlu_emotion_score, emotions)
     parameter_estimated_emotion = convert_score_to_emotion_category_and_name(
         emotion_score_applied_parameter, emotions
-    )
-    feedback_parameter_estimated_emotion = convert_score_to_emotion_category_and_name(
-        emotion_score_applied_feedback_parameter, emotions
     )
 
     response_object = {
