e0888bdabb34b48608fd9724ddbe60bc5d0f52d0
ShotaroOkada <b1016117@gmail.com>
2020-02-17 21:59:08 +0900

    より弱いより強いの更新ロジックを修正

--- src/domain/service/convert_score_to_emotion_category_and_name.py
+++ src/domain/service/convert_score_to_emotion_category_and_name.py
@@ -1,15 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from src.domain.entities.emotion import EmotionStrength
+from src.domain.service.sort_nlu_score import sort_nlu_score
 import pprint
 
 
 def convert_score_to_emotion_category_and_name(emotion_score, emotions):
-    max_emotion_category_score = -10
-
-    for emotion_category in emotion_score:
-        emotion_category_score = emotion_score[emotion_category]
-        if max_emotion_category_score < emotion_category_score:
-            max_emotion_category_score = emotion_category_score
-            max_emotion_category = emotion_category
+    sorted_emotion_score = sort_nlu_score(emotion_score)  # 型はタプル配列
+    max_emotion_category, max_emotion_category_score = sorted_emotion_score[0]
 
     print('selected emotion category is', max_emotion_category)
     if max_emotion_category_score < 0.34:
--- src/domain/service/update_emotion_parameter_to_user_evaluation.py
+++ src/domain/service/update_emotion_parameter_to_user_evaluation.py
@@ -12,7 +12,8 @@ def update_emotion_parameter_to_user_evaluation(nlu_score, emotion_parameter, us
     estimated_emotion_category = user_evaluation["emotion_category"]
     evaluation_id = user_evaluation["evaluation_id"]
     sorted_nlu_score = sort_nlu_score(nlu_score)  # 型はタプル配列
-    first_emotion_category, first_score = sorted_nlu_score[0]
+    _, first_score = sorted_nlu_score[0]
+    number_of_feature_words = len(feature_words)
 
     for feature_word in feature_words:
         if len(emotion_parameter):
@@ -20,14 +21,36 @@ def update_emotion_parameter_to_user_evaluation(nlu_score, emotion_parameter, us
                 if feature_word == word:
                     print('discovery same word: ', word)
                     if evaluation_id == "inappropriate":
-                        second_emotion_category, second_score = sorted_nlu_score[0]
-                        change_score = first_score - second_score + 0.000001
+                        _, second_score = sorted_nlu_score[0]
+                        change_score = (
+                            first_score - second_score + 0.000001) / number_of_feature_words
                         emotion_parameter[word][estimated_emotion_category] -= change_score
                     elif evaluation_id == "stronger":
-                        print("hoge")
+                        if first_score < 0.34:
+                            change_score = (0.34 - first_score) / \
+                                number_of_feature_words
+                            emotion_parameter[word][estimated_emotion_category] += change_score
+                        elif first_score < 0.67:
+                            change_score = (0.67 - first_score) / \
+                                number_of_feature_words
+                            emotion_parameter[word][estimated_emotion_category] += change_score
+                        else:
+                            print(
+                                "parameter is not change because stonger and 0.67 over")
                     elif evaluation_id == "weaker":
-                        for emotion_caterogy in entity_emotions:
-                            emotion_parameter[word][emotion_caterogy] += change_score
+                        if first_score < 0.34:
+                            print(
+                                "parameter is not change because weaker and 0.34 less")
+                        elif first_score < 0.67:
+                            change_score = (
+                                first_score - 0.34 - 0.000001) / number_of_feature_words
+                            for emotion_caterogy in entity_emotions:
+                                emotion_parameter[word][emotion_caterogy] -= change_score
+                        else:
+                            change_score = (
+                                first_score - 0.67 - 0.000001) / number_of_feature_words
+                            for emotion_caterogy in entity_emotions:
+                                emotion_parameter[word][emotion_caterogy] -= change_score
                     is_new_feature_word = False
         if(is_new_feature_word):
             print(feature_word, 'is new feature word')
@@ -35,16 +58,35 @@ def update_emotion_parameter_to_user_evaluation(nlu_score, emotion_parameter, us
                 new_word_parameter = copy.deepcopy(initial_word_parameter)
                 new_word_parameter["word"] = feature_word
                 if evaluation_id == "inappropriate":
-                    second_emotion_category, second_score = sorted_nlu_score[0]
-                    change_score = first_score - second_score + 0.000001
+                    _, second_score = sorted_nlu_score[0]
+                    change_score = (first_score - second_score +
+                                    0.000001) / number_of_feature_words
                     new_word_parameter[estimated_emotion_category] -= change_score
                 elif evaluation_id == "stronger":
-                    print("hoge")
-                elif evaluation_id == "weaker":
-                    for emotion_caterogy in entity_emotions:
+                    if first_score < 0.34:
+                        change_score = (0.34 - first_score) / \
+                            number_of_feature_words
                         new_word_parameter[emotion_caterogy] += change_score
-                        print('new score is ',
-                              new_word_parameter[emotion_caterogy])
+                    elif first_score < 0.67:
+                        change_score = (0.67 - first_score) / \
+                            number_of_feature_words
+                        new_word_parameter[emotion_caterogy] += change_score
+                    else:
+                        print(
+                            "new word parameter is not change because stonger and 0.67 over")
+                elif evaluation_id == "weaker":
+                    if first_score < 0.34:
+                        print("parameter is not change because weaker and 0.34 less")
+                    elif first_score < 0.67:
+                        change_score = (first_score - 0.34 -
+                                        0.000001) / number_of_feature_words
+                        for emotion_caterogy in entity_emotions:
+                            new_word_parameter[emotion_caterogy] -= change_score
+                    else:
+                        change_score = (first_score - 0.67 -
+                                        0.000001) / number_of_feature_words
+                        for emotion_caterogy in entity_emotions:
+                            new_word_parameter[emotion_caterogy] -= change_score
                 emotion_parameter[feature_word] = new_word_parameter
         is_new_feature_word = True
 
